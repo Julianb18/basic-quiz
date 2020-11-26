6694ce58c03494af7dda36560d11cb978a32f11f
Julianb18 <julian141718@gmail.com>
2020-11-26 21:32:28 +0100

    initialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Progress from "./components/Progress";
+import Question from "./components/Question";
+import Answers from "./components/Answers";
+import "./App.css";
 
 function App() {
+  const questions = [
+    {
+      id: 1,
+      question: "Which statement about Hooks is not true?",
+      answer_a:
+        "Hooks are 100% backwards-compatible and can be used side by side with classes",
+      answer_b: "Hooks are still in beta and not available yet",
+      answer_c:
+        "Hooks are completely opt-in, there's no need to rewrite existing code",
+      answer_d: "All of the above",
+      correct_answer: "b",
+    },
+    {
+      id: 2,
+      question: "Which one is not a Hook?",
+      answer_a: "useState()",
+      answer_b: "useConst()",
+      answer_c: "useReducer()",
+      answer_d: "All of the above",
+      correct_answer: "b",
+    },
+    {
+      id: 3,
+      question: "What Hook should be used for data fetching?",
+      answer_a: "useDataFetching()",
+      answer_b: "useApi()",
+      answer_c: "useEffect()",
+      answer_d: "useRequest()",
+      correct_answer: "c",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Progress total="3" current="1" />
+      <Question question="what is react?" />
+      <Answers />
+      <button className="btn btn-primary">Confirm & Continue</button>
     </div>
   );
 }
---
+++ src/components/Answers.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Answer from "./Answer";
+
+function Answers(props) {
+  return (
+    <>
+      <Answer letter="A" answer="Framework" />
+      <Answer letter="B" answer="Framework" />
+      <Answer letter="C" answer="Framework" />
+      <Answer letter="D" answer="Framework" />
+    </>
+  );
+}
+
+export default Answers;
