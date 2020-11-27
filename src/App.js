8b8c3e47874b9f4f1967fef8330dd05b1dd12539
Julianb18 <julian141718@gmail.com>
2020-11-27 14:46:08 +0100

    basic quiz app with usestate

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import "./App.css";
 function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [currentAnswer, setCurrentAnswer] = useState("");
+  const [answers, setAnswers] = useState([]);
+  const [showResults, setShowResults] = useState(false);
+  const [error, setError] = useState("");
 
   const questions = [
     {
@@ -44,19 +47,91 @@ function App() {
 
   const handleClick = (e) => {
     setCurrentAnswer(e.target.value);
+    setError("");
+    console.log(currentAnswer);
   };
-  return (
-    <div className="container">
-      <Progress total="3" current="1" />
-      <Question question={questionData.question} />
-      <Answers
-        answer={questionData}
-        currentAnswer={currentAnswer}
-        handleClick={handleClick}
-      />
-      <button className="btn btn-primary">Confirm & Continue</button>
-    </div>
-  );
+
+  const renderError = () => {
+    if (!error) {
+      return;
+    }
+    return <div className="error">{error}</div>;
+  };
+
+  const renderResultMark = (question, answer) => {
+    if (question.correct_answer === answer.answer) {
+      return <span className="correct">Correct</span>;
+    } else {
+      return <span className="failed">Failed</span>;
+    }
+  };
+
+  const renderResultsData = () => {
+    return answers.map((answer) => {
+      const question = questions.find(
+        (question) => question.id === answer.questionId
+      );
+      return (
+        <div key={question.id}>
+          {question.question} - {renderResultMark(question, answer)}
+        </div>
+      );
+    });
+  };
+
+  const restart = () => {
+    setAnswers([]);
+    setCurrentAnswer("");
+    setCurrentQuestion(0);
+    setShowResults(false);
+  };
+
+  const next = () => {
+    const answer = { questionId: questionData.id, answer: currentAnswer };
+
+    if (!currentAnswer) {
+      setError("Please select an answer");
+      return;
+    }
+    answers.push(answer);
+    setAnswers(answers);
+    setCurrentAnswer("");
+
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+      return;
+    }
+
+    setShowResults(true);
+  };
+
+  if (showResults) {
+    return (
+      <div className="container results">
+        <h2>Results</h2>
+        <ul>{renderResultsData()}</ul>
+        <button className="btn btn-primary" onClick={restart}>
+          Restart
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <Progress total={questions.length} current={currentQuestion + 1} />
+        <Question question={questionData.question} />
+        {renderError()}
+        <Answers
+          answer={questionData}
+          currentAnswer={currentAnswer}
+          handleClick={handleClick}
+        />
+        <button className="btn btn-primary" onClick={next}>
+          Confirm & Continue
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
